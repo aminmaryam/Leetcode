f41b7e0b407c184651b7bd688e6f6bc990b7d132
Maryam Amin <aminmaryam135@gmail.com>
2026-07-17 02:47:17 +0500

    Time: 0 ms (100%), Space: 8.7 MB (89.53%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+/*
+opening brackets will be directly pushed into into stack
+last opening bracket <=> first closing bracket
+
+*/
+    bool isValid(string str) {
+        stack<char> st;
+
+        for(int i=0; i<str.size(); i++){
+            if(str[i] == '(' || str[i] == '{' || str[i] == '['){ //opening
+                st.push(str[i]);
+            }
+            else{//closing
+                if(st.size() == 0){
+                    return false;
+                }
+                
+                if((st.top() == '(' && str[i] == ')' ) || 
+                    (st.top() == '{' && str[i] == '}') ||
+                    (st.top() == '[' && str[i] == ']')){
+                        st.pop();
+                    }
+
+                    else{//no match
+                        return false;
+                    }
+            }
+
+        }
+        return st.size() == 0;
+    }
+};
